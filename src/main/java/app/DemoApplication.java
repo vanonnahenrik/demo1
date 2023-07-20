7f48c05f6f0a05556b0d174b079fba99ee0e20d2
Henrik van Onna <Henrik.vanOnna@dsa.de>
2023-07-20 17:12:39 +0200

    init

---
+++ src/main/java/app/DemoApplication.java
@@ -0,0 +1,23 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+@RestController
+@RequestMapping
+class DemoController {
+    @GetMapping
+    public String hello() {
+        return "Hello World!";
+    }
+}
